c7949da6700cdbf6378f35bf8890dbbcda10db5e
arifrahman2901 <arifrahman2901@gmail.com>
2020-08-26 01:48:08 +0700

    projek kedua

---
+++ lib/config_web.php
@@ -0,0 +1,4 @@
+<?php
+$web_url = "http://localhost/olshopmaster/";
+$admin_url = "http://localhost/olshopmaster/adminpages/";
+?>
---
+++ pages/mail.php
@@ -0,0 +1,50 @@
+<div class="banner10" id="home1">
+		<div class="container">
+			<h2>Hubungi Kami</h2>
+		</div>
+	</div>
+<!-- //banner -->
+
+<!-- breadcrumbs -->
+	<div class="breadcrumb_dress">
+		<div class="container">
+			<ul>
+				<li><a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a> <i>/</i></li>
+				<li>Hubungi Kami</li>
+			</ul>
+		</div>
+	</div>
+<!-- //breadcrumbs -->
+
+<!-- mail -->
+	<div class="mail">
+		<div class="container">
+			<h3>Hubungi Kami</h3>
+			<div class="agile_mail_grids">
+				<div class="col-md-5 contact-left">
+					<h4>Alamat</h4>
+					<p>8lapanstore
+						<span>Jakarta Timur</span></p>
+					<ul>
+						
+						<li>Telepon : (+628)2125143901</li>
+						
+						<li><a href="mailto:arifrahman2901@gmail.com">mailto:arifrahman2901@gmail.com</a></li>
+					</ul>
+				</div>
+				<div class="col-md-7 contact-left">
+					<h4>Kontak</h4>
+					<form action="#" method="post">
+						<input type="text" name="Name" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+						<input type="email" name="Email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+						<input type="text" name="Telephone" value="Telephone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Telephone';}" required="">
+						<textarea name="message" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+						<input type="submit" value="Submit" >
+					</form>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+
+			
+		</div>
+	</div>
